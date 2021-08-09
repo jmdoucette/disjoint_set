0889e943967dba3a27f0754f9b2185f98feabc4b
jmdoucette <jmdoucette41@gmail.com>
2021-08-09 18:21:43 -0400

    another kruskal test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -47,7 +47,6 @@ fn test_kruskal() {
     ];
     let mut msf1 = kruskal(7, edges1);
     msf1.sort_by_key(|edge| (edge.u, edge.v));
-
     let expected1 = vec![
         Edge::new(0, 1, 7),
         Edge::new(0, 3, 5),
@@ -57,4 +56,21 @@ fn test_kruskal() {
         Edge::new(4, 6, 9),
     ];
     assert_eq!(msf1, expected1);
+
+    let edges2 = vec![
+        Edge::new(0, 1, -2),
+        Edge::new(0, 1, 3),
+        Edge::new(0, 2, 8),
+        Edge::new(0, 2, 100),
+        Edge::new(1, 2, 7),
+        Edge::new(1, 2, 7),
+    ];
+    let mut msf2 = kruskal(3, edges2);
+    msf2.sort_by_key(|edge| (edge.u, edge.v));
+    let expected2 = vec![
+        Edge::new(0, 1, -2),
+        Edge::new(1, 2, 7),
+    ];
+    assert_eq!(msf2, expected2);
+
 }
